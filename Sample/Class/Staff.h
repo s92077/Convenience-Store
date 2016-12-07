634b7ddd2970650bbdb8a84eec7eb9b46247b7a2
cloudhy417 <h23371197@gmail.com>
2016-12-07 17:58:18 +0800

    staff

--- Sample/Class/Staff.h
+++ Sample/Class/Staff.h
@@ -6,17 +6,21 @@ using namespace std;
 class Staff
 {
 public:
-	Staff();
+	Staff() { salary = 10000; shift = 1; };
+	Staff(int s1, int s2) { salary = s1; shift = s2; };
+
+	int get_salary() { return salary; }
+	int get_shift() { return shift; }
+
+	void set_salary(int s) { salary = s; }
+	void set_shift(int s) { shift = s; }
+
 	~Staff();
 
 private:
 	int salary;
 	int shift;//1. 6:00~12:00 2. 12.00~18:00 3. 18:00~24:00 4. 0:00~6:00 
 };
-
-Staff::Staff()
-{
-}
 
 Staff::~Staff()
 {
