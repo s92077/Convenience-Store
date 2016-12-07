87a578668a19c2949eb91d4503af3cf4e9767e88
s92077 <s92077@msn.com>
2016-12-07 12:00:51 +0800

    +product id +productname

--- Sample/Class/Product.h
+++ Sample/Class/Product.h
@@ -1,6 +1,7 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
 #include<iostream>
+#include"Stdafx.h"
 using namespace std;
 
 class Product
@@ -10,6 +11,8 @@ public:
 	~Product();
 
 private:
+	uint32_t id;
+	System::String ^productname;
 	int price; //price of the product
 	uint8_t status; //[on shelf][][][][][][][]
 	time_t arval_date;
