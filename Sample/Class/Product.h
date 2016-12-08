84f49d6aa01318dad9189526503704fc8c78d39d
s92077 <s92077@msn.com>
2016-12-08 15:05:36 +0800

    Constructor

--- Sample/Class/Product.h
+++ Sample/Class/Product.h
@@ -3,42 +3,63 @@
 #include<iostream>
 #include<string>
 #include<windows.h>
+#using <mscorlib.dll>
+using namespace System;
 
-
-class Product
+ref class Product
 {
 public:
 	Product();
-	Product(uint32_t, std::string);
+	Product(uint32_t, System::String^);
+	Product(uint32_t, System::String^, SYSTEMTIME);
+	Product(uint32_t, System::String^,SYSTEMTIME,SYSTEMTIME);
 	~Product();
-
 private:
 	uint32_t id;
-	std::string productname;
+	System::String^ productname;
 	int price; //price of the product
 	uint8_t status; //[on shelf][][][][][][][]
-	SYSTEMTIME arval_date;
-	SYSTEMTIME onshelf_date;
-	time_t a;
+	SYSTEMTIME *arval_date;
+	SYSTEMTIME *onshelf_date;
 };
 
-Product::Product(uint32_t id, std::string name)
-{
-	this->id = id;
-	this->productname = name;
 
-
-
-}
 Product::Product()
 {
 	this->id = -1;
 	this->productname = "";
-	
-
-
+	arval_date = new SYSTEMTIME;
+	*(this->arval_date) = arval;
+	onshelf_date = nullptr;
+}
+Product::Product(uint32_t id, System::String^ name)
+{
+	this->id = id;
+	this->productname = name;
+	arval_date = new SYSTEMTIME;
+	*(this->arval_date) = arval;
+	onshelf_date = nullptr;
+}
+Product::Product(uint32_t id, System::String^ name, SYSTEMTIME arval) {
+	this->id = id;
+	this->productname = name;
+	arval_date = new SYSTEMTIME;
+	*(this->arval_date) = arval;
+	onshelf_date = nullptr;
+}
+Product::Product(uint32_t id, System::String^ name, SYSTEMTIME arval, SYSTEMTIME onshelf) {
+	this->id = id;
+	this->productname = name;
+	arval_date = new SYSTEMTIME;
+	*(this->arval_date) = arval;
+	onshelf_date = new SYSTEMTIME;
+	*(this->arval_date) = onshelf;
 }
 Product::~Product()
 {
+	if (arval_date != nullptr) 
+		delete[] arval_date;
+	if (onshelf_date)
+		delete[] onshelf_date;
 }
 #endif
