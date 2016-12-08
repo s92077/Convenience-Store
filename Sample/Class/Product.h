84772ee9452c74a25e1be15e6e335bc41d5ca5e8
s92077 <s92077@msn.com>
2016-12-08 15:24:47 +0800

    set arval_time onshelf_time

--- Sample/Class/Product.h
+++ Sample/Class/Product.h
@@ -3,7 +3,6 @@
 #include<iostream>
 #include<string>
 #include<windows.h>
-#using <mscorlib.dll>
 using namespace System;
 
 ref class Product
@@ -11,8 +10,12 @@ ref class Product
 public:
 	Product();
 	Product(uint32_t, System::String^);
-	Product(uint32_t, System::String^, SYSTEMTIME);
+	Product(uint32_t, System::String^,SYSTEMTIME);
 	Product(uint32_t, System::String^,SYSTEMTIME,SYSTEMTIME);
+	SYSTEMTIME& get_arvaltime();
+	SYSTEMTIME& get_onshelftime();
+	void set_arvaltime(int year, int month, int day, int hour, int minute, int second);
+	void set_onshelftime(int year, int month, int day, int hour, int minute, int second);
 	~Product();
 private:
 	uint32_t id;
@@ -29,7 +32,7 @@ Product::Product()
 	this->id = -1;
 	this->productname = "";
 	arval_date = new SYSTEMTIME;
-	*(this->arval_date) = arval;
+	GetLocalTime(this->arval_date);
 	onshelf_date = nullptr;
 }
 Product::Product(uint32_t id, System::String^ name)
@@ -37,7 +40,7 @@ Product::Product(uint32_t id, System::String^ name)
 	this->id = id;
 	this->productname = name;
 	arval_date = new SYSTEMTIME;
-	*(this->arval_date) = arval;
+	GetLocalTime(this->arval_date);
 	onshelf_date = nullptr;
 }
 Product::Product(uint32_t id, System::String^ name, SYSTEMTIME arval) {
@@ -54,6 +57,27 @@ Product::Product(uint32_t id, System::String^ name, SYSTEMTIME arval, SYSTEMTIME
 	*(this->arval_date) = arval;
 	onshelf_date = new SYSTEMTIME;
 	*(this->arval_date) = onshelf;
+}
+void Product::set_arvaltime(int year, int month, int day, int hour, int minute, int second) {
+	(*(this->arval_date)).wYear = year;
+	(*(this->arval_date)).wMonth = month;
+	(*(this->arval_date)).wHour = hour;
+	(*(this->arval_date)).wMinute = minute;
+	(*(this->arval_date)).wSecond = second;
+
+}
+void Product::set_onshelftime(int year, int month, int day, int hour, int minute, int second) {
+	(*(this->onshelf_date)).wYear = year;
+	(*(this->onshelf_date)).wMonth = month;
+	(*(this->onshelf_date)).wHour = hour;
+	(*(this->onshelf_date)).wMinute = minute;
+	(*(this->onshelf_date)).wSecond = second;
+}
+inline SYSTEMTIME& Product::get_arvaltime() {
+	return *(this->arval_date);
+}
+inline SYSTEMTIME& Product::get_onshelftime() {
+	return *(this->onshelf_date);
 }
 Product::~Product()
 {
