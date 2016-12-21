a0aa6c16a8a5a4ec54c8fed981988005195c8ba8
cloudhy417 <h23371197@gmail.com>
2016-12-21 18:44:09 +0800

    staff&form

--- Sample/Class/Staff.h
+++ Sample/Class/Staff.h
@@ -11,15 +11,20 @@ public:
 
 	int get_salary() { return salary; }
 	int get_shift() { return shift; }
+	string get_id() { return id; }
+	string get_password() { return password; }
 
 	void set_salary(int s) { salary = s; }
 	void set_shift(int s) { shift = s; }
-
+	void set_id(string s) { id = s; }
+	void set_password(string s) { password = s; }
 	~Staff();
 
 private:
 	int salary;
 	int shift;//1. 6:00~12:00 2. 12.00~18:00 3. 18:00~24:00 4. 0:00~6:00 
+	string id;
+	string password;
 };
 
 Staff::~Staff()
--- Sample/Form1.h
+++ Sample/Form1.h
@@ -34,6 +34,14 @@ namespace Sample {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox2;
 
 	private:
 		/// <summary>
@@ -48,11 +56,79 @@ namespace Sample {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox2 = (gcnew System::Windows::Forms::RichTextBox());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(272, 80);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(975, 43);
+			this->textBox1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(1273, 66);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(173, 64);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"¬d¸ß";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(120, 80);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 38);
+			this->comboBox1->TabIndex = 3;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(119, 192);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(1327, 721);
+			this->richTextBox1->TabIndex = 4;
+			this->richTextBox1->Text = L"";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(272, 134);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(83, 30);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"label1";
+			// 
+			// richTextBox2
+			// 
+			this->richTextBox2->Location = System::Drawing::Point(1599, 66);
+			this->richTextBox2->Name = L"richTextBox2";
+			this->richTextBox2->Size = System::Drawing::Size(289, 847);
+			this->richTextBox2->TabIndex = 6;
+			this->richTextBox2->Text = L"";
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 30);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(2004, 999);
+			this->Controls->Add(this->richTextBox2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
