8283bc7330a1585dc04113b06bb493cecde92c6c
cloudhy417 <h23371197@gmail.com>
2017-01-13 16:43:02 +0800

    123

--- Sample/Form1.h
+++ Sample/Form1.h
@@ -117,7 +117,24 @@ namespace Sample {
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::Button^  button4;
 	private: System::Windows::Forms::Button^  button5;
-			 /// </summary>
+private: System::Windows::Forms::Panel^  panel1;
+private: System::Windows::Forms::Panel^  panel2;
+private: System::Windows::Forms::TextBox^  textBox2;
+private: System::Windows::Forms::Label^  label7;
+private: System::Windows::Forms::Label^  label6;
+private: System::Windows::Forms::Label^  label5;
+private: System::Windows::Forms::Label^  label4;
+private: System::Windows::Forms::Label^  label3;
+private: System::Windows::Forms::Label^  label2;
+private: System::Windows::Forms::Label^  label1;
+private: System::Windows::Forms::Button^  button6;
+private: System::Windows::Forms::TextBox^  textBox8;
+private: System::Windows::Forms::TextBox^  textBox7;
+private: System::Windows::Forms::TextBox^  textBox6;
+private: System::Windows::Forms::TextBox^  textBox5;
+private: System::Windows::Forms::TextBox^  textBox4;
+private: System::Windows::Forms::TextBox^  textBox3;
+		 /// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
@@ -145,19 +162,38 @@ namespace Sample {
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(339, 80);
+			this->textBox1->Location = System::Drawing::Point(232, 13);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(975, 43);
 			this->textBox1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(1414, 77);
+			this->button1->Location = System::Drawing::Point(1324, 13);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(173, 64);
 			this->button1->TabIndex = 1;
@@ -168,9 +204,9 @@ namespace Sample {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(120, 80);
+			this->comboBox1->Location = System::Drawing::Point(16, 13);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(168, 38);
+			this->comboBox1->Size = System::Drawing::Size(193, 38);
 			this->comboBox1->TabIndex = 3;
 			// 
 			// dataGridView1
@@ -180,7 +216,7 @@ namespace Sample {
 				this->流水號, this->品名,
 					this->價格, this->數量, this->到店時間, this->到期日, this->烹飪時間, this->溫度
 			});
-			this->dataGridView1->Location = System::Drawing::Point(120, 170);
+			this->dataGridView1->Location = System::Drawing::Point(16, 97);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersVisible = false;
 			this->dataGridView1->RowTemplate->Height = 45;
@@ -246,7 +282,7 @@ namespace Sample {
 			// checkBox1
 			// 
 			this->checkBox1->AutoSize = true;
-			this->checkBox1->Location = System::Drawing::Point(120, 130);
+			this->checkBox1->Location = System::Drawing::Point(16, 57);
 			this->checkBox1->Name = L"checkBox1";
 			this->checkBox1->Size = System::Drawing::Size(171, 34);
 			this->checkBox1->TabIndex = 6;
@@ -261,6 +297,7 @@ namespace Sample {
 			this->button2->TabIndex = 7;
 			this->button2->Text = L"查詢功能";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
 			// button3
 			// 
@@ -270,6 +307,7 @@ namespace Sample {
 			this->button3->TabIndex = 8;
 			this->button3->Text = L"進貨功能";
 			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &Form1::button3_Click);
 			// 
 			// button4
 			// 
@@ -289,26 +327,183 @@ namespace Sample {
 			this->button5->Text = L"人員管理";
 			this->button5->UseVisualStyleBackColor = true;
 			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->panel2);
+			this->panel1->Controls->Add(this->dataGridView1);
+			this->panel1->Controls->Add(this->comboBox1);
+			this->panel1->Controls->Add(this->checkBox1);
+			this->panel1->Controls->Add(this->textBox1);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Location = System::Drawing::Point(95, 94);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1659, 831);
+			this->panel1->TabIndex = 11;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->button6);
+			this->panel2->Controls->Add(this->textBox8);
+			this->panel2->Controls->Add(this->textBox7);
+			this->panel2->Controls->Add(this->textBox6);
+			this->panel2->Controls->Add(this->textBox5);
+			this->panel2->Controls->Add(this->textBox4);
+			this->panel2->Controls->Add(this->textBox3);
+			this->panel2->Controls->Add(this->label7);
+			this->panel2->Controls->Add(this->label6);
+			this->panel2->Controls->Add(this->label5);
+			this->panel2->Controls->Add(this->label4);
+			this->panel2->Controls->Add(this->label3);
+			this->panel2->Controls->Add(this->label2);
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->textBox2);
+			this->panel2->Location = System::Drawing::Point(0, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(1656, 825);
+			this->panel2->TabIndex = 12;
+			// 
+			// button6
+			// 
+			this->button6->Font = (gcnew System::Drawing::Font(L"新細明體", 12));
+			this->button6->Location = System::Drawing::Point(754, 719);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(187, 65);
+			this->button6->TabIndex = 14;
+			this->button6->Text = L"確認";
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &Form1::button6_Click);
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(668, 220);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(435, 43);
+			this->textBox8->TabIndex = 13;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(668, 304);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(435, 43);
+			this->textBox7->TabIndex = 12;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(668, 388);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(435, 43);
+			this->textBox6->TabIndex = 11;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(668, 478);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(435, 43);
+			this->textBox5->TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(668, 553);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(435, 43);
+			this->textBox4->TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(668, 628);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(435, 43);
+			this->textBox3->TabIndex = 8;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(526, 628);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(73, 30);
+			this->label7->TabIndex = 7;
+			this->label7->Text = L"溫度";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(526, 220);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(73, 30);
+			this->label6->TabIndex = 6;
+			this->label6->Text = L"價格";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(526, 304);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(73, 30);
+			this->label5->TabIndex = 5;
+			this->label5->Text = L"數量";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(526, 388);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(133, 30);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"到店日期";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(526, 478);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(103, 30);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"到期日";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(530, 553);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(133, 30);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"烹飪時間";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(526, 135);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(73, 30);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"品名";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(668, 135);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(435, 43);
+			this->textBox2->TabIndex = 0;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(16, 30);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(2216, 992);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
-			this->Controls->Add(this->checkBox1);
-			this->Controls->Add(this->dataGridView1);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->textBox1);
 			this->MinimizeBox = false;
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -363,6 +558,71 @@ namespace Sample {
 			delete cmd;
 		}
 	}
+private: System::Void button6_Click(System::Object^  sender, System::EventArgs^  e) {
+	bool same = false;
+	try
+	{
+		/* 連結開啟 */
+		connect->Open();
+
+		/* 傳送指令到 MySQL */
+		//
+		strSQL = "SELECT * FROM 便利商店系統 WHERE 品名= '" + textBox2->Text + "'";//MySQL command you want to use
+							  //
+		cmd = gcnew MySqlCommand(strSQL, connect);
+		
+		reader = cmd->ExecuteReader();
+		if (reader->Read())
+			same = true;
+		connect->Close();
+		delete cmd;
+	}
+	catch (Exception ^ex)
+	{
+		System::Windows::Forms::DialogResult result;
+		result = MessageBox::Show(ex->ToString());
+		connect->Close();
+		delete cmd;
+	}
+	
+	
+		try
+		{
+			if (same) {
+				/* 連結開啟 */
+				connect->Open();
+
+				/* 傳送指令到 MySQL */
+				//
+
+				strSQL = "update 便利商店系統 set 數量 = 數量+"+ textBox7->Text +" where 品名='" + textBox2->Text + "';";//MySQL command you want to use															   //
+				cmd = gcnew MySqlCommand(strSQL, connect);
+				reader = cmd->ExecuteReader();
+				connect->Close();
+				System::Windows::Forms::DialogResult result;
+				result = MessageBox::Show("新增成功");
+				delete cmd;
+			}
+		}
+		catch (Exception ^ex)
+		{
+			System::Windows::Forms::DialogResult result;
+			result = MessageBox::Show(ex->ToString());
+			connect->Close();
+			delete cmd;
+		}
+
+
+	
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	panel1->Visible = true;
+	panel2->Visible = false;
+}
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+	panel1->Visible = false;
+	panel2->Visible = true;
+}
 };
 }
 
